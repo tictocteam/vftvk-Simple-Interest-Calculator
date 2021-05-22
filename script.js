bbc0fe12bbbae5bb08943614efdf54f1b9f35a5a
tictocteam <tim.bartlett@fachmann.com.au>
2021-05-22 11:41:44 +0000

    script.js

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function compute()
     var years = document.getElementById("years").value;
     var amount = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+
-        ",\<br\>at an interest rate of "+rate+
-        "%.\<br\>You will receive an amount of "+amount+
-        ",\<br\>in the year "+year+"\<br\>";  
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+
+        "</mark>,\<br\>at an interest rate of <mark>"+rate+
+        "%</mark>.\<br\>You will receive an amount of <mark>"+amount+
+        "</mark>,\<br\>in the year <mark>"+year+"</mark>.\<br\>";  
 }
         
