acfc78e1699ff9fd83c7e22a7691fdf8e910ca6e
tictocteam <tim.bartlett@fachmann.com.au>
2021-05-23 12:15:07 +0000

    almost ready to submit

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-function checkinput()
-{
-    var principal = document.getElementById("principal").value;
-    if(principal==""){
-        alert("Please enter a valid amount.");
-        principal.focus();
-        return false;
-    }
-    if(principal<=0){
-        alert("Please enter a valid amount.");
-        principal.focus();
-        return false;
-    }
-}
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
-function compute()
+
+function checkinput()
 {
+    var principal = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var amount = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+
-        "</mark>,\<br\>at an interest rate of <mark>"+rate+
-        "%</mark>.\<br\>You will receive an amount of <mark>"+amount+
-        "</mark>,\<br\>in the year <mark>"+year+"</mark>.\<br\>";  
-}
-        
+    if(principal=="" || principal<=0){
+        alert("Please enter a valid amount.");
+        document.getElementById("principal").innerText=reset;
+        document.getElementById("result").innerHTML="";
+        document.getElementById("principal").focus();
+        return false;
+    }else{ 
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+
+            "</mark>,\<br\>at an interest rate of <mark>"+rate+
+            "%</mark>.\<br\>You will receive an amount of <mark>"+amount+
+            "</mark>,\<br\>in the year <mark>"+year+"</mark>.\<br\>";      
+    }
+}    
+          
